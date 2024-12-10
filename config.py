e1cb1baced3665ac291531341c758dbec68576e7
gybzd <98418402+gybzd@users.noreply.github.com>
2024-12-11 01:11:12 +0200

    Create config.py for hosting related configs

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+import threading
+import asyncio
+import streamlit as st
+
+# Helper to retrieve environment variables
+def get_token():
+    if os.environ.get("HOSTING") == "replit":
+        return os.environ["TOKEN"]
+    elif os.environ.get("HOSTING") == "streamlit":
+        return st.secrets["TOKEN"]
+    else:
+        raise RuntimeError("HOSTING environment variable not set or invalid!")
+
+# Helper to run the bot
+def run_asyncio_bot(application):
+    if os.environ.get("HOSTING") == "replit":
+        asyncio.run(application.run_polling(allowed_updates=["message"]))
+    elif os.environ.get("HOSTING") == "streamlit":
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        thread = threading.Thread(target=loop.run_until_complete, args=(application.run_polling(allowed_updates=["message"]),), daemon=True)
+        thread.start()
+    else:
+        raise RuntimeError("Unsupported hosting configuration.")
